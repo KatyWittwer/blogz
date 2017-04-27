c8850a8bfadd8c0421de3023d4bb543c7b12e0ee
KatyWittwer <katy.k.wittwer@gmail.com>
2017-04-27 09:33:02 -0500

    completed assignment criteria and tested

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ class BlogHandler(webapp2.RequestHandler):
     def get_posts_by_user(self, user, limit, offset):
         """
             Get all posts by a specific user, ordered by creation date (descending).
-            The user parameter will be a User object.
-        """
-
-        # TODO - filter the query so that only posts by the given user
-        return None
+            The user parameter will be a User object."""
+        query = Post.all().filter("author", user).order("-created")
+        return query.fetch(limit=limit, offset=offset)
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
